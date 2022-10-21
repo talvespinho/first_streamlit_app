897baf303dcfe897545971b12591b0ac53ffcd09
talvespinho <115723240+talvespinho@users.noreply.github.com>
2022-10-21 14:20:36 +0200

    Update streamlit_app.py
    pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,3 +10,8 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.header('\N{mechanical arm} Breakfast of Champion Coders \N{flexed biceps}')
+
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
