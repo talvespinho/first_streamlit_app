f435939cc9122c91842e6957d843a689731f0f1c
talvespinho <115723240+talvespinho@users.noreply.github.com>
2022-10-21 14:23:56 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,8 @@ streamlit.title('My new pet')
 
 streamlit.header('cat')
 streamlit.text('it is like a kow')
+
+
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
@@ -15,6 +17,7 @@ streamlit.header('\N{mechanical arm} Breakfast of Champion Coders \N{flexed bice
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
+my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
